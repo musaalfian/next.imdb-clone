a63760b9d21d043a7e1e88821b9077ed8354c37b
musaalfian <musaganteng071@@gmail.com>
2024-06-02 21:41:32 +0700

    refactor: styling card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
+import { FaStar } from 'react-icons/fa';
+
+const RoundNumber = ({ value }) => {
+  const roundedValue = parseFloat(value).toFixed(1);
+  return <span className='text-gray-900 dark:text-gray-400'>{roundedValue}</span>;
+};
 
 export default function Card({ result, index }) {
   console.log(result);
   return (
-    <div key={index} className='p-3 border rounded-lg border-slate-300'>
+    <div key={result.id} className='border border-gray-400 dark:border-none dark:bg-gray-800'>
       <img
         src={`http://image.tmdb.org/t/p/original/${result.poster_path}`}
-        className='object-cover max-h-[240px] sm:max-h-[300px] w-full h-full overflow-hidden rounded-lg'
+        className='object-cover max-h-[240px] sm:max-h-[300px] w-full h-full overflow-hidden'
         width={200}
         height={300}
         alt={result.title}
       />
-      <h3 className='mt-4 mb-2 text-base font-bold'>{result.title || result.name}</h3>
-      <p className='line-clamp-2 text-slate-300'>{result.overview}</p>
-      <div className='mt-1 text-gray-300 '>
-        <p>{result.release_date}</p>
-        <p>Popularity: {result.popularity}</p>
+      <div className='p-3'>
+        <div className='mt-1 mb-2 text-gray-300 '>
+          <div className='flex items-center gap-2'>
+            <FaStar className='text-amber-400' />
+            <RoundNumber value={result.vote_average} />
+          </div>
+        </div>
+        <h3 className='mb-1 text-base font-bold'>{result.title || result.name}</h3>
+        <p className='text-gray-700 dark:text-gray-400 line-clamp-2'>{result.overview}</p>
       </div>
     </div>
   );
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -3,7 +3,7 @@ import Card from './Card';
 
 export default function Results({ results }) {
   return (
-    <div className='grid max-w-6xl grid-cols-1 gap-3 p-3 mx-auto mt-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+    <div className='grid max-w-6xl grid-cols-1 min-[420px]:grid-cols-2 gap-5 p-3 mx-auto mt-8 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
       {results.length > 0 ? (
         results.map((result, index) => <Card result={result} index={index} />)
       ) : (
