92fd416e2b9c53b8cf2aed7df534dd0b4a4472fd
musaalfian <musaganteng071@@gmail.com>
2024-06-06 23:52:21 +0700

    feat: add suspense wrapper on navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import NavbarItem from './NavbarItem';
 
 export default function Navbar() {
   return (
     <>
       <div className='flex items-center justify-center gap-6 p-3 bg-amber-100 dark:bg-slate-700'>
-        <NavbarItem title='Trending' param='fetchTrending' />
-        <NavbarItem title='Top Rated' param='fetchTopRated' />
+        <Suspense>
+          <NavbarItem title='Trending' param='fetchTrending' />
+          <NavbarItem title='Top Rated' param='fetchTopRated' />
+        </Suspense>
       </div>
     </>
   );
