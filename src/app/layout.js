778e41b5f71d7b9287591d4ab6d86c201ca1086a
musaalfian <musaganteng071@@gmail.com>
2024-06-06 23:31:08 +0700

    feat: add suspense wrapper element

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import Header from '@/components/Header';
 import Providers from './Providers';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
+import { Suspense } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,12 +17,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <Providers>
-          <Header />
-          <Navbar />
-          {children}
-          <Footer />
-        </Providers>
+        <Suspense>
+          <Providers>
+            <Header />
+            <Navbar />
+            {children}
+            <Footer />
+          </Providers>
+        </Suspense>
       </body>
     </html>
   );
